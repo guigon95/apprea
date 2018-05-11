be9fa248dd9f2330a8edf8741e2456be96704369
Guilherme Gonçalves <guilherme_gon@live.com>
2018-05-11 12:57:05 -0300

    commit

--- config.php
+++ config.php
@@ -13,7 +13,7 @@ if ( !defined('ABSPATH') )
 	
 /** caminho no server para o sistema **/
 if ( !defined('BASEURL') )
-	define('BASEURL', 'https://apprea.herokuapp.com/');
+	define('BASEURL', '/apprea/'); /**'https://apprea.herokuapp.com/');
 	
 /** caminho do arquivo de banco de dados **/
 if ( !defined('DBAPI') )
