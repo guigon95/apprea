13caaa734706a836707afd59adc747e756637bcb
Guilherme Gonçalves <guilherme_gon@live.com>
2018-05-11 01:49:59 -0300

    test

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Ver DEploy - Recursos Educacionais Abertos - Boas práticas em Engenharia de Software</title>
+  <title>Ver- Recursos Educacionais Abertos - Boas práticas em Engenharia de Software</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
