f06bb7767b4f7d54ca11c67772f00093b50c0c9e
Guilherme Gonçalves <guilherme_gon@live.com>
2018-05-17 04:17:54 -0300

    commit

--- database.php
+++ database.php
@@ -1,6 +1,6 @@
 <?php 
 
-	$pdo = new PDO('mysql:host=localhost;dbname=apprea', 'root', 'root');
+	$pdo = new PDO('mysql:host=us-cdbr-iron-east-04.cleardb.net;dbname=heroku_7ce8565c5f375ca', 'b86e0689e91634', '7e72b5c8');
 	$pdo->exec("SET CHARACTER SET utf8");	
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
