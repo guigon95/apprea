dbd88284c233cc8cd9d462a4c6ddd561c9527718
Guilherme Gonçalves <guilherme_gon@live.com>
2019-08-13 23:06:07 -0300

    questionarios

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -69,13 +69,13 @@
         <li class="treeview">
           <a href="#">
             <i class="fa fa-files-o"></i>
-            <span>Formulários</span>
+            <span>Questionários</span>
             <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="https://forms.gle/vUFqTa2bd13BUEqv5"><i class="fa fa-circle-o"></i>Questionário 1</a></li>
+            <li><a href="https://forms.gle/vUFqTa2bd13BUEqv5"><i class="fa fa-circle-o"></i>Q1</a></li>
           </ul>
         </li>
 
