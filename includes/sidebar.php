3decfe4db7de47aabdb0a0c17a7e80683021a37a
Guilherme Gonçalves <guilherme_gon@live.com>
2019-08-13 23:04:02 -0300

    questionario

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -66,6 +66,18 @@
           </ul>
         </li>
 
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-files-o"></i>
+            <span>Questionário</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="https://forms.gle/vUFqTa2bd13BUEqv5"><i class="fa fa-circle-o"></i>Questionário</a></li>
+          </ul>
+        </li>
 
           <?php if($_SESSION['admin']==1){ ?>
         <li class="treeview">
