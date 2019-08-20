1c6cfa3412d2611728fe31131b89210dacd3e304
Guilherme Gonçalves <guilherme_gon@live.com>
2019-08-20 13:11:58 -0300

    Validacao Fases

--- pages/asyncPages/valida_fase.php
+++ pages/asyncPages/valida_fase.php
@@ -28,9 +28,18 @@
 		$rs = $GLOBALS['pdo']->prepare($sql);
 		$rs->execute();	
 
-		$sql = "update usuario_has_fase set flag_fase = 1 where id_usuario = ".$_SESSION['id_usuario']." and id_fase = (SELECT id_fase from fase where numero_fase > '".$_POST['numero_fase']."' and id_area = '".$_POST['id_area']."' order by id_fase asc limit 1)";
-		$rs = $GLOBALS['pdo']->prepare($sql);
-		$rs->execute();	
+
+		if ($_POST['id_area'] == 12) {
+			$sql = "update usuario_has_fase set flag_fase = 1 where id_usuario = ".$_SESSION['id_usuario']." and id_fase = (SELECT id_fase from fase where numero_fase = '".$_POST['numero_fase']."' and id_area = '22' order by id_fase asc limit 1)";
+			$rs = $GLOBALS['pdo']->prepare($sql);
+			$rs->execute();	
+		}
+		else if ($_POST['id_area'] == 22) {
+			$sql = "update usuario_has_fase set flag_fase = 1 where id_usuario = ".$_SESSION['id_usuario']." and id_fase = (SELECT id_fase from fase where numero_fase > '".$_POST['numero_fase']."' and id_area = '12' order by id_fase asc limit 1)";
+			$rs = $GLOBALS['pdo']->prepare($sql);
+			$rs->execute();
+		}
+			
 
 		if($_POST['numero_fase'] == 1 && $_POST['id_area'] == 12 && $_SESSION['id_patente'] == 2){
 			$sql = "update usuario set flag_patente = 1, id_patente = 12 where id_usuario = ".$_SESSION['id_usuario'];
