c914f508dd52456c086b003f6bfcb6e228fe1340
Guilherme Gonçalves <guilherme_gon@live.com>
2019-08-30 16:26:39 -0300

    valida fase

--- pages/asyncPages/valida_fase.php
+++ pages/asyncPages/valida_fase.php
@@ -29,11 +29,11 @@
 		$rs->execute();	
 
 
-		if ($_POST['id_area'] == 12) {
-			$sql = "update usuario_has_fase set flag_fase = 1 where id_usuario = ".$_SESSION['id_usuario']." and id_fase = (SELECT id_fase from fase where numero_fase = '".$_POST['numero_fase']."' and id_area = ". $_POST['id_area'] ." order by id_fase asc limit 1)";
+		//if ($_POST['id_area'] == 12) {
+			$sql = "update usuario_has_fase set flag_fase = 1 where id_usuario = ".$_SESSION['id_usuario']." and id_fase = (SELECT id_fase from fase where numero_fase > '".$_POST['numero_fase']."' and id_area = ". $_POST['id_area'] ." order by id_fase asc limit 1)";
 			$rs = $GLOBALS['pdo']->prepare($sql);
 			$rs->execute();	
-		}
+		//}
 		/*else if ($_POST['id_area'] == 22) {
 			$sql = "update usuario_has_fase set flag_fase = 1 where id_usuario = ".$_SESSION['id_usuario']." and id_fase = (SELECT id_fase from fase where numero_fase > '".$_POST['numero_fase']."' and id_area = '12' order by id_fase asc limit 1)";
 			$rs = $GLOBALS['pdo']->prepare($sql);
