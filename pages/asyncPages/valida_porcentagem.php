39595c71cfbcd75422246ace274de5211139039e
Guilherme Gonçalves <guilherme_gon@live.com>
2019-10-22 22:26:55 -0300

    commit

--- pages/asyncPages/valida_porcentagem.php
+++ pages/asyncPages/valida_porcentagem.php
@@ -2,20 +2,11 @@
 
 	require_once ('../../database.php');
 
-	session_start();
-	
-		
-
-		
+	session_start();	
 
 		$sql = "update usuario_has_fase set porcentagem = ".$_POST['porcentagem']." where id_usuario = ".$_SESSION['id_usuario']." and id_fase = ".$_POST['id_fase'];
 		$rs = $GLOBALS['pdo']->prepare($sql);
 		$rs->execute();	
 
 
-		 
-		
-		
-
-	
  ?>	
--- pages/asyncPages/valida_testes.php
+++ pages/asyncPages/valida_testes.php
@@ -12,14 +12,7 @@
 		 
 		$sql = "update usuario_has_fase set flag_fase = 1 where id_usuario = ".$_SESSION['id_usuario']." and id_fase = (SELECT id_fase from fase where numero_fase > '".$_POST['numero_fase']."' and id_area = ".$_POST['idarea']." order by id_fase asc limit 1)";
 		$rs = $GLOBALS['pdo']->prepare($sql);
-		$rs->execute();	
-
-
-		
-
-
-		 
-		 
+		$rs->execute();		 
 		}
 		
 
